5a86a34f2dd43f04e75d400368bc8294cebc8652
KoryKGreen <37316121+raifhun@users.noreply.github.com>
2018-06-25 11:23:01 -0400

    Add files via upload

--- HDF5 Extraction Tool Set.py
+++ HDF5 Extraction Tool Set.py
@@ -54,11 +54,52 @@ def spectrum_extract(current_file):  # extract the histograms and sum them per a
                 line_to_write.append(current_file[group+"/"+current_set].attrs.get(attribute))
             if x == 1:
                 line_to_write = line_to_write[:-1]
-            line_to_write.append(current_file[group+"/"+current_set][0:1998:1].sum())
+            line_to_write.append(current_file[group+"/"+current_set][0:2000:1].sum())
             local_data_formatted.append([line_to_write])
             line_to_write = []
             # print(local_data_formatted)
     return local_data_formatted
+
+
+def specific_extract(current_file, wl_start, wl_end):  # extract the histograms and sum them per attribute
+    group_set = gather_groups(current_file)
+    local_data_formatted = []
+    line_to_write = []
+    hist_totals = []
+    wl_range = []
+    last_set = ""
+    for group in group_set:
+        sets = gather_sets(current_file, group)
+        x = 0
+        for current_set in sets:
+            print(current_set)
+            if x == 0:
+                hist_totals = []
+                for j in range(0, len(current_file[group + "/" + current_set])):
+                    hist_totals.append([j, 0])
+                print(hist_totals)
+
+                wl_range.append(current_file[group + "/" + current_set].attrs.get("Em"))
+                line_to_write.append(str(current_file).rsplit('/', 1)[-1])
+                for i in range(0, 2):
+                    line_to_write.append(" , ")
+                local_data_formatted.append(line_to_write)
+            x += 1
+            line_to_write = []
+            # check wavelength and add if true
+            if current_file[group + "/" + current_set].attrs.get("Em") >= wl_start and \
+                    current_file[group+"/" + current_set].attrs.get("Em") <= wl_end:
+                for i in range(0, len(current_file[group + "/" + current_set])):
+                    hist_totals[i][1] += current_file[group + "/" + current_set][i]
+            last_set = current_set
+        wl_range.append(current_file[group + "/" + last_set].attrs.get("Em"))
+        local_data_formatted.append(["Wavelength Start", "Wavelength End"])
+        local_data_formatted.append([wl_start, wl_end])
+        print(wl_range)
+        for line in hist_totals:
+            local_data_formatted.append(line)
+
+        return local_data_formatted
 
 
 def decay_extract(current_file):  # extracts the hdf5 data in a format to read out the decay characteristics
@@ -166,9 +207,11 @@ def main_extraction_loop():  # logic that runs the extraction, can be replaced w
         write_to_file(total_data_entry, dat_file_final, file_list)
         print("Files successfully Extracted")
     if int(process_type) == 3:  # if process 3, runs the decay extraction for specific wavelengths per group
+        wl_start = int(input("What wavelength would you like to start on?"))
+        wl_end = int(input("What wavelength would you like to stop on?"))
         for file in file_list:
-            total_data_entry.append(specific_extract(file))
-        write_to_file(total_data_entry, dat_file_final)
+            total_data_entry.append(specific_extract(file, wl_start, wl_end))
+        write_to_file(total_data_entry, dat_file_final, file_list)
         print("Files successfully Extracted")
 
 
